71a72429a49804132c75f448a55e90125d07fd90
Christopher Agu <119140336+aguchristopher@users.noreply.github.com>
2025-04-18 23:41:13 +0100

    Update adminAuth.js

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 module.exports = async (req, res, next) => {
- /* try {
-    const token = req.headers.authorization?.split(' ')[1];
+  try {
+    /* const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
       return res.status(401).json({ message: 'Authentication required' });
     }
