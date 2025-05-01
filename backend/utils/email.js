d03d116744960b0371517697e75f03329e70315f
Christopher Agu <119140336+aguchristopher@users.noreply.github.com>
2025-05-01 06:37:21 +0100

    Update email.js

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'mail.privateemail.com', // Namecheap's SMTP host
+  host: 'stmp.privateemail.com', // Namecheap's SMTP host
   port: 465, // Secure port for SSL
   secure: true,
   auth: {
