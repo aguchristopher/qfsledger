c1be4a65f3b604223a302da5f3152b7f62c16ab6
Christopher Agu <119140336+aguchristopher@users.noreply.github.com>
2025-05-10 14:44:20 +0100

    Update email.js

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,12 +1,22 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
+const transporterr = nodemailer.createTransport({
   // Configure your email service here
   // Example for Gmail:
   service: 'gmail',
   auth: {
     user: 'aguchris740@gmail.com',
     pass: 'thswpkcauzjpwvzk'
+  }
+});
+
+var transporter = nodemailer.createTransport({
+  host: "mail.privateemail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'support@qfsbestsecure.info',
+    pass: 'Donkay101$'
   }
 });
 
